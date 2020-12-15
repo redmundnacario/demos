db598a58d9346ccd63faefabd7e295f6f821a4d3
redmundnacario <redmund.nacario@gmail.com>
2020-12-16 03:07:36 +0800

    add new buttons

--- html_and_css/Assignment-5-Calculator/js/button.js
+++ html_and_css/Assignment-5-Calculator/js/button.js
@@ -2,56 +2,107 @@
 
 const initialValue = 0;
 
-var screenValue = document.querySelector(".screen-value");
+let screenValue = document.querySelector(".screen-value");
 screenValue.innerText = initialValue;
 
 // if number  and operations was pressed
 
-function numKeyPressed() {
-    if (screenValue.innerText  == initialValue){
+const numKeyPressed = function () {
+    // check if new value input is equal to zero
+    // let lastChar = screenValue.innerText.substr(screenValue.innerText.length - 1);
+
+    if (screenValue.innerText == initialValue ){
         screenValue.innerText  = this.innerText;
+        // console.log(screenValue.innerText)
     } else {
         screenValue.innerText = screenValue.innerText  + this.innerText;
+        // console.log(screenValue.innerText)
     }
 }
 
+//  if dot was pressed 
+
+const dotPressed = function () {
+    // if last item in screen is dot also
+    let lastChar = screenValue.innerText.substr(screenValue.innerText.length - 1);
+
+    if (lastChar  == this.innerText){ 
+        // default
+        alert("A period '.' cannot be followed by another period.")
+        return
+    };
+
+    //if last number has dot already
+    // Uses Regex
+    let wholeExp = screenValue.innerText.split(/[+/÷/×/-]/)
+    let lastExpValue = wholeExp[wholeExp.length - 1]
+    
+    if (lastExpValue.includes(".")){ 
+        alert("Cannot add period '.' again for the current value.")
+        return 
+    } 
+        
+    screenValue.innerText = screenValue.innerText  + this.innerText;
+    
+};
+
+// Clear Screen
+
+const clearPressed = function () {
+    screenValue.innerText = initialValue;
+};
+
 // delete previous
 
-function deletePrevious() {
+const deletePrevious = function () {
     if(screenValue.innerText.length == 1 ){
         screenValue.innerText = initialValue;
     } else {
         screenValue.innerText = screenValue.innerText.slice(0, -1);
-    }
-}
+    };
+};
+
 // run equal sign or perform the operation
 
-function equalSignPressed() {
+const equalSignPressed = function () {
     
     var sVBucket = screenValue.innerText;
 
     if (sVBucket.includes("×")){
-        sVBucket = sVBucket.replace("×", "*")
+        sVBucket = sVBucket.replaceAll("×", "*")
     };
     if (sVBucket.includes("÷")){
-        sVBucket = sVBucket.replace("÷", "/")
+        sVBucket = sVBucket.replaceAll("÷", "/")
     };
 
-    console.log(sVBucket);
+    // console.log(sVBucket);
     try {
         var result =  eval(sVBucket);
+        
+        if (Number.isInteger(result)){
+            //integer
+            screenValue.innerText = result;
+        } else {
+            // float
+            if (String(result).length >= 8){
+                // Round off
+                result = result.toPrecision(7);
+            } 
+
+            screenValue.innerText = result;
+        }
         
         screenValue.innerText = result;
     }
     catch(err) {
         alert("Kindly check your input. \nError: " + err.message);
-    }
-}
+    };
+};
 
 
 // Assign the functions into event listeners in selected elements
 
-var items = document.querySelectorAll(".numKey");
+let items = document.querySelectorAll(".numKey");
 // console.log(items)
 for (let item of items) {
     // console.log(item.querySelector(".button-text").innerText)
@@ -63,3 +114,9 @@ deleteButton.addEventListener("click", deletePrevious);
 
 const equalButton = document.querySelector(".equal");
 equalButton.addEventListener("click", equalSignPressed);
+
+const dotButton = document.querySelector(".dot");
+dotButton.addEventListener("click", dotPressed);
+
+const clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", clearPressed);
