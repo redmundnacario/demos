d729332a0ffbb0e392caca45ee86ed9725242ed7
redmundnacario <redmund.nacario@gmail.com>
2020-12-20 23:55:55 +0800

    Pawn successfully moved initially

--- lab_works/lab-4-chess-game/js/draw-tiles.js
+++ lab_works/lab-4-chess-game/js/draw-tiles.js
@@ -1,43 +1,106 @@
 // alert("draw-tiles.js connected")
-let rowNumbers = 8;
-let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-        
-const app = document.getElementById("app");
-let chessObj = {}
 
-for (let i=rowNumbers; i > 0 ; i--){
+export const DrawChessTiles = function () {
+    // Our main state/data for mapping chess pieces for every state change
+    let chessObj = {};
+
+    // Initializations of chess map
+    let rowNumbers = 8;
+    let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
     
-    if (i%2 == 0){
-        var colorInd = true;
-    } else {
-        var colorInd = false;
-    };
-    
-    // Create box
-    for (let j=0; j < rowNumbers; j++){
-        // console.log("column "+j)
-        if (colorInd == true){
-            var colorBox = "white";
-            colorInd = false;
+    // Get the main parent element which is a grid
+    const app = document.getElementById("app");
+
+    // For loop per chess row
+    for (let i=rowNumbers; i > 0 ; i--){
+        
+        // Dictates what color to start per row
+        if (i%2 == 0){
+            var colorInd = true;
         } else {
-            var colorBox = "black";
-            colorInd = true;
+            var colorInd = false;
         };
-
-        let chessBox =letters[j]+i;
-        const box = document.createElement("div");
-        box.setAttribute("class", "chess-box "+colorBox +" " + chessBox);
-        box.setAttribute("id", chessBox);
-        app.appendChild(box);
         
-        chessObj[chessBox] = {
-            color: colorBox,
-            rowNumber : i,
-            colLetter : letters[j],
-            colNumber : j,
-            piece: null,
+        // For loop chess box per row
+        for (let j=0; j < rowNumbers; j++){
+            
+            // Dictates the color per box in a row
+            if (colorInd == true){
+                var colorBox = "white";
+                colorInd = false;
+            } else {
+                var colorBox = "black";
+                colorInd = true;
+            };
+
+            let chessBox =letters[j]+i;
+            //  Create element for chess box
+            const box = document.createElement("div");
+
+            // Set the class name list and id per box 
+
+            // per class in class list denotes what style in css
+            box.setAttribute("class", "chess-box "+colorBox +" " + chessBox);
+            // the purpose of id is for locating the chess piece
+            box.setAttribute("id", chessBox);
+            // append to our main parent element
+            app.appendChild(box);
+            
+            // set the state of our chess object
+            chessObj[chessBox] = {
+                rowNumber : i,
+                colLetter : letters[j],
+                colNumber : j,
+                piece: null,
+            };
         };
     };
 
-    
+    return chessObj;
 };
+
+// insert the initial chess piece
+export const setChessPieces = (chessObj, piecesInitialPlace) => {
+    let keys = Object.keys(piecesInitialPlace);
+    for (let key in keys){
+        // Destructuring object
+        let {location, htmlcode, kingdom, position} = piecesInitialPlace[keys[key]];
+        // console.log(typeof location);
+        // if piece is multiple 
+        if (typeof location == "object"){
+            for(let rowKey in location){
+                chessObj[location[rowKey]].piece = { htmlcode,
+                                                     kingdom,
+                                                     position}
+            };
+        } else {
+            chessObj[location].piece = { htmlcode,
+                                         kingdom,
+                                         position}
+            
+        }
+    };
+    return chessObj;
+};
+
+// draw pieces
+export const drawChessPieces = (chessObj) => {
+    let keys = Object.keys(chessObj);
+    for (let chessBox in keys){
+        const chessBoxSelected = document.getElementById(keys[chessBox]);
+        const chessPieceBox = document.createElement("div");
+        chessPieceBox.innerHTML = (chessObj[keys[chessBox]].piece != null) ? (chessObj[keys[chessBox]].piece.htmlcode) : "" ;
+        chessPieceBox.setAttribute("class","chess-piece");
+        chessBoxSelected.appendChild(chessPieceBox);
+    };
+}
+
+// export const RedrawChessPieces = (chessObj) => {
+//     let keys = Object.keys(chessObj);
+//     for (let chessBox in keys){
+//         const chessBoxSelected = document.getElementById(keys[chessBox]).children;
+//         // console.log(chessBoxSelected)
+//         chessBoxSelected.innerHTML = (chessObj[keys[chessBox]].piece != null) ? (chessObj[keys[chessBox]].piece.htmlcode) : "" ;
+//         // chessBoxSelected.setAttribute("class","chess-piece");
+//     };
+// }
--- lab_works/lab-4-chess-game/js/moves.js
+++ lab_works/lab-4-chess-game/js/moves.js
@@ -1,39 +1,148 @@
+import { DrawChessTiles , setChessPieces, drawChessPieces} from './draw-tiles.js';
+import { CHESS_DATA } from "./chess-pieces.js";
 
 
-const DetermineChessPiece = function(){
+// Initializations
+let activeChessPlayer = "white";
+document.getElementById("activePlayer").innerHTML = activeChessPlayer.toUpperCase();
+let chessObj = DrawChessTiles();
+chessObj = setChessPieces(chessObj, CHESS_DATA);
+
+// Draw initial chess pieces in the map
+drawChessPieces(chessObj);
+// console.log(chessObj)
+
+// 
+const PawnMoves = function(chessObjBox) {
+    let rowNumbers = []
+    let {rowNumber, colNumber, colLetter} = chessObjBox
+    console.log(activeChessPlayer, rowNumber)
+    if (activeChessPlayer == "white") {
+        if(rowNumber == 2){
+            for (let i = rowNumber + 1; i < rowNumber + 3; i++) { rowNumbers.push(i); };
+        } else {
+            for (let i = rowNumber + 1; i < rowNumber + 2; i++) { rowNumbers.push(i); };
+        }
+    } else {
+        if(rowNumber == 7){
+            for (let i = rowNumber - 1; i < rowNumber - 3; i--) { rowNumbers.push(i); };
+        } else {
+            for (let i = rowNumber - 1; i < rowNumber - 2; i--) { rowNumbers.push(i); };
+        }
+    }
+    
+    return rowNumbers.map((value) => colLetter + value)
+}
+
+const PossibleMoves = function (piece, chessObjBox) {
+    // console.log(piece.position);
+
+    switch (piece.position) {
+        case "pawn":
+            return PawnMoves(chessObjBox)
+        default:
+            return null
+    }
+}
+
+const DetermineChessPiece = function(chessObj, chessBoxSelId){
+    let pieceSelected = chessObj[chessBoxSelId].piece;
+    const moves = PossibleMoves(pieceSelected, chessObj[chessBoxSelId]);
+    // console.log(moves);
+    return moves
+}
+
+const PossibleMoveSelected = function(previousBox, nextBox) {
+    // let chessBoxSelId = this.id;
+    // let classes =this.classList;
+    
+    console.log(previousBox + " to "+ nextBox);
+
+    // update chessObj
+    chessObj[nextBox].piece = chessObj[previousBox].piece;
+    console.log(chessObj[nextBox].piece);
+    chessObj[previousBox].piece = null;
+
+    console.log(chessObj[previousBox].piece);
+    console.log(chessObj);
+
+    // Redraw  chess pieces in the map
+    console.log(document.getElementById(previousBox).children[0].innerHTML)
+    document.getElementById(previousBox).children[0].innerHTML = "";
+    document.getElementById(nextBox).children[0].innerHTML = chessObj[nextBox].piece.htmlcode;
+
+    if (document.getElementsByClassName("selected").length > 0) {
+            
+        Object.keys(document.getElementsByClassName("selected")).forEach(value => 
+            {
+                document.getElementsByClassName("selected")[0].classList.remove("selected")
+            })
+        Object.keys(document.getElementsByClassName("possible-move")).forEach(value => 
+            {
+                document.getElementsByClassName("possible-move")[0].classList.remove("possible-move")
+            })
+    }
 
 }
 
-
 const ToggleActivePiece = function() {
-    let classes =this.classList
-    
+    let chessBoxSelId = this.id;
+    let classes =this.classList;
+
     // Toggle selected chess piece
     if( classes.value.includes("selected")) {
         this.classList.remove("selected");
+
+        Object.keys(document.getElementsByClassName("possible-move")).forEach(value => 
+            {
+                document.getElementsByClassName("possible-move")[0].classList.remove("possible-move")
+            })
     } else {
-        console.l
         // remove other chess piece with class 'selected'
         if (document.getElementsByClassName("selected").length > 0) {
-            document.getElementsByClassName("selected")[0].classList.remove("selected");
+            
+            Object.keys(document.getElementsByClassName("selected")).forEach(value => 
+                {
+                    document.getElementsByClassName("selected")[0].classList.remove("selected")
+                })
+            Object.keys(document.getElementsByClassName("possible-move")).forEach(value => 
+                {
+                    document.getElementsByClassName("possible-move")[0].classList.remove("possible-move")
+                })
         }
         // add 'selected' in class
         this.classList.add("selected");
 
-        // Determine what piece
-        console.log(this.innerText)
-        // Determine possible moves
+        // Determine chess piece and possible moves
+        let moves = DetermineChessPiece(chessObj, chessBoxSelId)
+        moves.forEach((move) => {document.getElementById(move).classList.add("possible-move")})
+
+        //add event listener to possible move squares
+        // console.log()
+        if (moves){
+            let possibleBox = document.getElementsByClassName("possible-move")
+            Object.keys(possibleBox).forEach(value =>{
+                possibleBox[value].addEventListener("click", () => {
+                    PossibleMoveSelected(chessBoxSelId , possibleBox[value].id)
+                } );
+            })
+        }
     };
 };
 
+// White or Black? determine first 'who's turn'
+
+
 // add event listener to each chess box
 let keys = Object.keys(chessObj);
-for (key in keys){
+for (let key in keys){
     // if chessbox  piece is missing in the chessObj... skip
     if (chessObj[keys[key]]["piece"] != null) {
-        const chessBoxSelected = document.getElementById(keys[key]);
-        chessBoxSelected.addEventListener("click", ToggleActivePiece);
-        console.log(keys[key])
+        if ( chessObj[keys[key]].piece.kingdom == activeChessPlayer){
+            const chessBoxSelected = document.getElementById(keys[key]);
+            chessBoxSelected.addEventListener("click", ToggleActivePiece);
+            // console.log(keys[key])
+        }
     }
 };
 
