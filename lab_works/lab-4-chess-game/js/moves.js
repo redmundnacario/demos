7b28b4c4dcacb51e07a8a10737431dd2212bb84f
redmundnacario <redmund.nacario@gmail.com>
2020-12-21 23:42:55 +0800

    initially set moves for knight

--- lab_works/lab-4-chess-game/js/moves.js
+++ lab_works/lab-4-chess-game/js/moves.js
@@ -1,10 +1,47 @@
 import PawnMoves from './pawn.js';
 const KnightMoves= (chessObjBox, chessObj, letters) => {
     console.log("knight", chessObjBox);
+    let {rowNumber, colNumber, colLetter, piece} = chessObjBox;
+    console.log(rowNumber,colNumber)
+    // possible moves
+    const scanPoints = [[2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2], [1, 2]];
+    // Convert to chess alpha numeric coordinates
+    let scanPointsAlphaNum = scanPoints.map(value => {
+        // Y to abc, X to 123
+        value = letters[(value[1]  + colNumber) - 1] + (value[0] + rowNumber);
+        // filter out of bounds
+        if (value[1] > 8 & value[1] > 0){
+            return null;
+        }
+        return value;
+    }).filter(Boolean);
+    console.log(scanPointsAlphaNum)
+
+    let possibleMoves = [];
+    let possibleTargets = [];
+
+    // classify to possible moves or targets
+    scanPointsAlphaNum.forEach(value => {
+        if (chessObj[value].piece == null){
+            possibleMoves.push(value)
+        } else {
+            if (chessObj[value].piece.kingdom != piece.kingdom) {
+                possibleTargets.push(value)
+            }
+        }
+    });
+
+    return { possibleMoves, possibleTargets}
 };
 
 const RookMoves= (chessObjBox, chessObj, letters) => {
     console.log("rook", chessObjBox);
+    // scan possible moves
+    // set target and set unpassable behind target
+
+    // filter out of bounds
+
+    // filter chess targets which are allies
 };
 const BishopMoves= (chessObjBox, chessObj, letters) => {
     console.log("bishop", chessObjBox);
