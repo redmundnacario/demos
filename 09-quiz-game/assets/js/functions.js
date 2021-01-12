265b7b4a33577c0ab2632d80f152994e0d335a76
redmundnacario <redmund.nacario@gmail.com>
2021-01-13 05:27:01 +0800

    quiz game finished

--- 09-quiz-game/assets/js/functions.js
+++ 09-quiz-game/assets/js/functions.js
@@ -8,12 +8,18 @@ export function Questions() {
     this.answers = answers_array,
     
     // html dom elements
+    this.cardDiv = document.querySelector(".card");
+    this.cardBottomDiv = document.querySelector(".card-bottom")
+    this.scoreH1 = document.getElementById("scoreH1");
+
     this.questionNoH1 = document.getElementById("questionNo");
     this.questionStatementH2 = document.getElementById("question");
     this.choicesInputRadio = document.getElementById("options");
     this.alertDiv = document.getElementById("msgContainer");
     this.alertMsgP = document.getElementById("message");
-    
+    this.exitPage = document.getElementById("exitPage")
+    this.exitMsgP = document.getElementById("exitMessage")
+
     // html Button elements
     this.exitBtn = document.getElementById("exit");
     this.submitBtn;
@@ -23,6 +29,7 @@ export function Questions() {
     this.question_index = null,
     this.score_int = 0; // for acc
     this.performance_array = []; // for detecting streak
+    this.round_int = 1;// no of rounds, for detecting streak
     
     // Create element
     this.createButtonSubmit = function () {
@@ -30,11 +37,28 @@ export function Questions() {
         btn.type = "button"
         btn.value = "Submit"
         this.submitBtn = btn;
-        this.choicesInputRadio.appendChild(btn)
+        // this.choicesInputRadio.appendChild(btn)
+        this.cardBottomDiv.appendChild(btn)
     }
 
+
+    // Main functions
+
     this.InitializeApp = function() {
-        
+        this.alertDiv.style.visibility = 'hidden';
+        let optionsChild = Array.from(this.choicesInputRadio.children)
+        optionsChild.forEach(child => child.remove());
+        this.getRandomQuestion()
+    }
+
+    this.exitApp = function() {
+        this.alertDiv.style.visibility = 'hidden';
+        // remove element of card to hidden
+        this.cardDiv.style.visibility = 'hidden';
+        // create another elements  such as total score 
+        this.exitPage.style.visibility= 'visible';
+        this.exitMsgP.innerText = "Your Total Score: " + this.score_int;
+
     }
 
     this.getRandomInt = function() {
@@ -75,29 +99,69 @@ export function Questions() {
         // this.answer  = prompt("Choose the number of correct answer.");
     },
 
+    this.displayScore = function(){
+        console.log("Your Score: ", this.score_int)
+    }
+
+    this.determineStreak = function(){
+        
+        if (this.round_int%5 == 0){
+            let streak_array = this.performance_array.slice(
+                (this.performance_array.length - this.round_int),
+                this.performance_array.length
+            )  
+            
+            let sumStreak = streak_array.reduce((accum,value) => {
+                return value+accum},0)
+            
+            if (sumStreak == this.round_int){
+                console.log(sumStreak + " Winning Streak")
+                alert(sumStreak + " Correct Answers in a Row!")
+            }
+                
+        }
+    }
+
     this.verifyAnswer = function(){
 
-        console.log("Your Answer: ", this.answer, " correctAnswer: ",this.answers[this.question_index]);
+        console.log("Your Answer: ", this.answer);
 
         // Verify Answer
         if (this.answer === this.answers[this.question_index]){
             console.log( "Correct!")
             
             //Set alert display and and content
+            this.alertDiv.classList.remove("alert-danger")
             this.alertDiv.classList.add("alert-success")
             this.alertDiv.style.visibility = 'visible';
             this.alertMsgP.innerText = "Correct! Your Answer: " + 
                 this.choices[this.question_index][this.answer - 1]
 
+            this.score_int++
+            this.performance_array.push(1)
+            
+            // determine if 5, 10, 15 ... streak
+            this.determineStreak()
+
         } else {
             console.log( "Wrong!")
+            this.alertDiv.classList.remove("alert-success")
             this.alertDiv.classList.add("alert-danger")
             this.alertDiv.style.visibility = 'visible';
             this.alertMsgP.innerText = "Wrong! Your Answer: " + 
                 this.choices[this.question_index][this.answer - 1]
+
+            this.performance_array.push(0)
         }
-        
-        // Reinitialize after 1 second
+
+        this.round_int++
+        this.displayScore();
+        this.scoreH1.innerText = "Score: "+ this.score_int
+
+        // Reinitialize after 3 second
+        setTimeout(() => {
+            this.InitializeApp()
+        }, 3000);
     }
 
     this.submitAnswer = function(){
--- 09-quiz-game/assets/js/index.js
+++ 09-quiz-game/assets/js/index.js
@@ -3,9 +3,11 @@ import { Questions } from './functions.js';
 document.body.onload = (event) => {
     const App = new Questions()
     // initialize
-    App.getRandomQuestion();
+    App.InitializeApp();
     // create submit button once
     App.createButtonSubmit();
     // Add event on submit button
     App.submitBtn.onclick = () => App.submitAnswer();
+
+    App.exitBtn.onclick = () => App.exitApp();
 };
